d240fdb7b912728a05947d9ec2ef94c7f3e6f767
Marc Santa <mrchrome@gmail.com>
2026-03-20 08:31:32 -0400

    Initial Commit

---
+++ MJView/MJViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MJViewApp.swift
+//  MJView
+//
+//  Created by Marc Santa on 3/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MJViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
